fbbec3363c8e3f0ff3da2bcdb5de2b7f45d55fe3
Rongjian Zhang <pd4d10@gmail.com>
2025-12-17 00:58:33 +0800

    ci: add typecheck and lint check job

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,14 @@
 // @ts-check
 
+import { defineConfig } from 'eslint/config'
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
+export default defineConfig([
   {
     ignores: ['**/dist/**', '**/node_modules/**', 'packages/**'],
   },
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
-  {
-    rules: {
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    },
-  },
-)
+])
